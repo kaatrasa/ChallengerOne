242f177c2db95b8c68e4cc95aff9b770d0f55abf
valtsuPC <kaatrasa@student.tut.fi>
2019-01-27 16:28:15 +0200

    using std::chrono for time now

--- timeman.h
+++ timeman.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#include <windows.h>
+#include <chrono>
 
 #include "utils/defs.h"
 
 namespace Timeman {
 	inline int get_time() {
-		return GetTickCount();
+		return std::chrono::duration_cast<std::chrono::milliseconds>
+			(std::chrono::steady_clock::now().time_since_epoch()).count();
 	}
 
 	inline void check_time_up(SearchInfo& info) {
